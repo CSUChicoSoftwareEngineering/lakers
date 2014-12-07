fa7adc8333abd5ec46ea98fab7f033f156291857
Jason Boutte <boutte.jason@gmail.com>
2014-12-07 13:28:00 -0800

    Adds student association to grade

--- include/data/grade.h
+++ include/data/grade.h
@@ -12,14 +12,17 @@ class Grade {
     wxString Id() const { return m_id; }
     wxString Value() const { return m_value; }
     wxString AssessmentId() const { return m_assessmentId; }
+		wxString StudentId() const { return m_studentId; };
 
     void SetValue(const wxString &value) { m_value = value; }
     void SetAssessmentId(const wxString &aid) { m_assessmentId = aid; }
+		void SetStudentId(const wxString &sid) { m_studentId = sid; }
 
   private:
     wxString m_id;
     wxString m_assessmentId;
     wxString m_value;
+		wxString m_studentId;
 };
 
 #endif
